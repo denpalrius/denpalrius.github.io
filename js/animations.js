069ef80f4de65438824eebd20c7744d4b703330a
Dennis Riungu <2971971+denpalrius@users.noreply.github.com>
2025-06-30 22:02:47 -0400

    Made experiencer section smoother

--- js/animations.js
+++ js/animations.js
@@ -10,7 +10,7 @@ document.addEventListener('DOMContentLoaded', () => {
     
     const observerOptions = {
       threshold: 0.1,
-      rootMargin: '0px 0px -50px 0px'
+      rootMargin: '0px 0px 50px 0px'
     };
   
     const observer = new IntersectionObserver((entries) => {
